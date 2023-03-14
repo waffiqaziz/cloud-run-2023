9b5674380d5991323bce9ea3e3a7e4df4d4d14ac
Waffiq Aziz <59906896+waffiqaziz@users.noreply.github.com>
2023-03-14 08:43:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send('Cloud Run | Tuesday 14, March 2023!');
 });
 
 const PORT = process.env.PORT || 8080;
